b758712c0d37a088e9906d3a1bb783827e8ae03d
Shah-Zaman <shahzamanlive2@gmail.com>
2024-01-18 11:46:10 +0500

    Update app.controller.spec.ts

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -7,7 +7,7 @@ describe('AppController', () => {
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      // controllers: [AppController],
+      controllers: [AppController],
       providers: [AppService],
     }).compile();
 
